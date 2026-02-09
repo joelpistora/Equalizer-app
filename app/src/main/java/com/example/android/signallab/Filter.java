ad53d97f1937509c3ce51995837219e373a438d1
davidtang <davidtang.punkt@gmail.com>
2026-02-09 19:43:07 +0100

    Made audio engine and biquad filter class

---
+++ app/src/main/java/com/example/android/signallab/Filter.java
@@ -0,0 +1,61 @@
+package com.example.android.signallab;
+
+public class Filter {
+    private double a0,a1,a2, b0, b1,b2;
+    private double z1 = 0, z2 = 0;
+    public enum Type {
+        LOWPASS,
+        BANDPASS,
+        HIGHPASS
+    }
+    public Filter(double SampleRate, double freq, double q, Type type){
+        setCoefficients(
+                SampleRate,
+                freq,
+                q,
+                type
+        );
+    }
+    private void setCoefficients(double SampleRate, double freq, double q, Type type){
+        double omega = 2 * Math.PI * freq / SampleRate;
+        double alpha = Math.sin(omega) / (2*q);
+        double cosw = Math.cos(omega);
+
+        switch(type) {
+            case LOWPASS:
+                b0 = (1-cosw)/2;
+                b1 = 1-cosw;
+                b2 = (1-cosw)/2;
+                a0 = 1 + alpha;
+                a1 = -2*cosw;
+                a2 = 1 - alpha;
+                break;
+            case BANDPASS:
+                b0 = alpha;
+                b1 = 0;
+                b2 = -alpha;
+                a0 = 1 + alpha;
+                a1 = -2*cosw;
+                a2 = 1 - alpha;
+                break;
+            case HIGHPASS:
+                b0 = (1 + cosw)/2;
+                b1 = -(1 + cosw);
+                b2 = (1 + cosw)/2;
+                a0 = 1 + alpha;
+                a1 = -2*cosw;
+                a2 = 1 - alpha;
+        }
+    }
+    public float process(float x){
+        double y = ((b0/a0)*x + (b1/a0)*z1 + (b2/a0)*z2 - (a1/a0)*z1 - (a2/a0)*z2);
+        z2 = z1;
+        z1 = x;
+
+        return (float)y;
+    }
+    public void reset(){
+        z1 = 0;
+        z2 = 0;
+    }
+}
