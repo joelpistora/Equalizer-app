931fa45cd30f5599a26814d7fa2ab3a45b1be5cc
davidtang <davidtang.punkt@gmail.com>
2026-02-11 10:56:29 +0100

    commit UI

--- app/src/main/java/com/example/android/signallab/EQActivity.java
+++ app/src/main/java/com/example/android/signallab/EQActivity.java
@@ -13,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class EQActivity extends AppCompatActivity {
     private static final int REQUEST_AUDIO_FILE = 2001;
-
+    private static final String TAG = "EQEngine";
     private AudioEngine audioEngine;
 
     private SeekBar bassBar, midBar, trebleBar;
@@ -30,6 +30,7 @@ public class EQActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eq);
+
 
         // Connect to UI later, I've only done bassbar. The UI is ugly so I gave up for now
         bassBar = findViewById(R.id.bassBar);
